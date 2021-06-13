ac42d2d1dfab5ccc7f91387ea71df43b77f6f6df
epo-maker <epo4956@gmail.com>
2021-06-13 22:14:14 +1000

    init

---
+++ KaggleAssignment/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A model to predict if a rookie will last atleast 5 years in the league',
+    author='Advance Data Science Team',
+    license='MIT',
+)
